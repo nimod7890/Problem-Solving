046fec3888b19a62ba831d967670f6e4f5e18c6a
nimod7890 <nimod1234@g.skku.edu>
2022-03-12 00:49:33 +0900

    min heap ^^

---
+++ 1927 최소 힙.py
@@ -0,0 +1,22 @@
+import heapq
+import sys
+
+
+input = sys.stdin.readline
+
+minheap = []
+
+
+n = int(input())
+num = 0
+for i in range(n):
+    x = int(input())
+    if x > 0:
+        heapq.heappush(minheap, x)
+        num += 1
+    elif x == 0:
+        if num == 0:
+            print(0)
+        else:
+            print(heapq.heappop(minheap))
+            num -= 1
