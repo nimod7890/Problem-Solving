806bb48ce4f84d297b2d483e7db2c96a7d2266f9
nimod7890 <nimod1234@g.skku.edu>
2022-03-17 00:22:35 +0900

    어제 멈칫한 실3 마무리 ... ~~

--- 1966 프린터 큐.py
+++ 1966 프린터 큐.py
@@ -1,8 +1,21 @@
+from collections import deque
 import sys
-
 
 input = sys.stdin.readline
 t = int(input())
 for i in range(t):
     n, m = map(int, input().split())
-    important = [[idx, x] for idx, x in enumerate(map(int, input().split()))]
+    nlist = [[idx, x] for idx, x in enumerate(map(int, input().split()))]
+    important = sorted(nlist[:], reverse=True, key=lambda x: x[1])
+
+    cnt = 0
+    while True:
+        x = nlist.pop(0)
+        if x[1] < important[0][1]:  # 뒤에 재배치
+            nlist.append(x)
+            continue
+        cnt += 1
+        if m == x[0]:
+            break
+        important.pop(0)
+    print(cnt)
