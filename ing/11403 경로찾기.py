3f5d108fac7722e66792de114637f5d21f431e4c
nimod7890 <nimod1234@g.skku.edu>
2022-04-02 21:12:02 +0900

    Add .gitignore

--- ing/11403 경로찾기.py
+++
@@ -1,4 +0,0 @@
-n = int(input())
-graph = [list(map(int, input().split())) for i in range(n)]
-for i in graph:
-    print(i)
--- ing/2193 이친수.py
+++
@@ -1,9 +0,0 @@
-# 시간 초과 시...발
-
-n=int(input())
-startNum=int('1'+'0'*(n-1),2)
-endNum=int('1'+'0'*n,2)
-cnt=0
-for num in range(startNum,endNum):
-    if '11' in str(bin(num)):cnt +=1  
-print(cnt)
