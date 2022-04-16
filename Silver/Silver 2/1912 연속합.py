8bc402284ebda265de1955f77ec4cb08e30370c2
nimod7890 <nimod1234@g.skku.edu>
2022-04-16 21:41:47 +0900

    여행와서 커밋하기

---
+++ Silver/Silver 2/1912 연속합.py
@@ -0,0 +1,10 @@
+import sys
+
+input = sys.stdin.readline
+n = int(input())
+nlist = list(map(int, input().split()))
+dp = [0]*n
+dp[0] = nlist[0]
+for i in range(1, n):
+    dp[i] = max(dp[i-1]+nlist[i], nlist[i])
+print(max(dp))
