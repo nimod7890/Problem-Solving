70e8d7b47dd83b78be4382bced6f556aeecc46a1
nimod7890 <nimod1234@g.skku.edu>
2022-04-28 22:09:50 +0900

    틀려서 고쳣슴

--- Silver/Silver 1/14501 퇴사.py
+++ Silver/Silver 1/14501 퇴사.py
@@ -1,12 +1,11 @@
 import sys
 
-
 input = sys.stdin.readline
 n = int(input())
 nlist = [list(map(int, input().split())) for i in range(n)]
-dp = [0]*n
-for i in reversed(range(n-1)):
-    if i+nlist[i][0] > n-1:
+dp = [0]*(n+1)
+for i in reversed(range(n)):
+    if i+nlist[i][0] > n:
         dp[i] = dp[i+1]
     else:
         dp[i] = max(dp[i+1], nlist[i][1]+dp[i+nlist[i][0]])
