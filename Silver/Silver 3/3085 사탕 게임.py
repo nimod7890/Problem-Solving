8d7a8d6a5a21fa74accc2531659ae14e2f5687d0
nimod7890 <nimod1234@g.skku.edu>
2022-05-06 19:28:16 +0900

    오랜만에 제대로 문풀

--- Silver/Silver 3/3085 사탕 게임.py
+++ Silver/Silver 3/3085 사탕 게임.py
@@ -1,18 +1,36 @@
-from xmlrpc.client import MAXINT
+import sys
 
-
+input = sys.stdin.readline
 n = int(input())
-nList = [list(input()) for i in range(n)]
+nList = [list(input().strip()) for i in range(n)]
 maxi = 0
 
 
 def check(x, y, X, Y):
     global maxi
     nList[x][y], nList[X][Y] = nList[X][Y], nList[x][y]
-    for i in range(n):
-        for j in range(n):
-            pass
+    for i in range(0, n):
+        last = 0
+        row = [1]
+        for j in range(1, n):
+            if nList[i][j] == nList[i][j-1]:
+                row[last] += 1
+            else:
+                last += 1
+                row.append(1)
+        maxi = max(max(row), maxi)
 
+    for j in range(0, n):
+        last = 0
+        column = [1]
+        for i in range(1, n):
+            if nList[i][j] == nList[i-1][j]:
+                column[last] += 1
+            else:
+                last += 1
+                column.append(1)
+        maxi = max(max(column), maxi)
+    nList[x][y], nList[X][Y] = nList[X][Y], nList[x][y]
     return maxi
 
 
@@ -21,3 +39,4 @@ for i in range(n):
         for x, y in [[i + 1, j], [i, j + 1]]:
             if 0 <= x < n and 0 <= y < n and nList[x][y] != nList[i][j]:
                 maxi = check(x, y, i, j)
+print(maxi)
