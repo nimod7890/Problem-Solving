a259eef4d983b3e9c8f9c214367426764aeddc69
nimod7890 <nimod1234@g.skku.edu>
2022-09-28 00:29:29 +0900

    .

--- Silver/Silver 3/1463 1로 만들기(1).py
+++ Silver/Silver 3/1463 1로 만들기(1).py
@@ -1,30 +1,9 @@
-
-from collections import defaultdict, deque
-
-
 n=int(input())
-nlist=defaultdict()
-will=deque()
-if n%3==0:
-    tmp=n
-    cnt=0
-    while tmp%3==0:
-        cnt+=1
-        tmp//=3
-    will.append([tmp,cnt])
-    print([tmp,cnt])
-will.append([n-1,1])
-ans=[]
-while will:
-    n,cnt=will.popleft()
-    if n==1: 
-        ans.append(cnt)
-    if n<1: continue
-    if n%3==0:
-        will.appendleft([n//3,cnt+1])
-    elif n%2==0:
-        will.append([n//2,cnt+1])
-    else:
-        will.append([n-1,cnt+1])
-    print(will)
-print(min(ans))
+dp=[0]*(n+1)
+for i in range(2,n+1):
+    dp[i]=dp[i-1]+1
+    if i%3==0:
+        dp[i]=min(dp[i],dp[i//3]+1)
+    if i%2==0:
+        dp[i]=min(dp[i],dp[i//2]+1)
+print(dp[n])
