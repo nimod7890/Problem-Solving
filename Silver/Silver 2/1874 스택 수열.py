603a14db78e425fd429da7ae1a0b73de85cae44e
nimod7890 <nimod1234@g.skku.edu>
2023-02-04 03:47:52 +0900

    week 3

--- Silver/Silver 2/1874 스택 수열.py
+++ Silver/Silver 2/1874 스택 수열.py
@@ -4,18 +4,18 @@ n=int(input())
 stack=[int(input()) for i in range(n)]
 end=[0]*n
 curr=0
-string=""
+string=[]
 for get in stack: 
     if get>curr:
         for i in range(curr,get):
             if end[i]==0:
-                string+='+'
+                string.append('+')
                 end[i]+=1
             curr+=1
     if get<=curr:
         for i in range(get-1,curr):
             if end[i]==1:
-                string+='-'
+                string.append('-')
                 end[i]-=2
             elif end[i]==-1:
                 end[i]-=1
