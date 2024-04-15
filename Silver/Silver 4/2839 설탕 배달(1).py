5ade4685d3d0447476cd40b12f1813a0ed1c7d9e
nimod7890 <nimod1234@g.skku.edu>
2024-04-15 14:33:49 +0900

    2839

--- Silver/Silver 4/2839 설탕 배달(1).py
+++ Silver/Silver 4/2839 설탕 배달(1).py
@@ -1,17 +1,10 @@
-from collections import deque
-
 n=int(input())
-will=deque([[n,0]])
-visited=[0]*(n+1)
-while will:
-  kg,num=will.popleft()
-  if kg==0:
-    print(num)
+num=0
+while n:
+  if n%5==0:
+    print(num+n//5)
     break
-  if kg<0 or visited[kg]:
-    continue
-  
-  visited[kg]=1
-  will.extend([[kg%5,num+kg//5],[kg-3,num+1]])
+  n-=3
+  num+=1
 else:
   print(-1)
