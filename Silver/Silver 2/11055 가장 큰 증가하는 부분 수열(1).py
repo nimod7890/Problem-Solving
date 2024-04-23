bdb7ec123f787cd411b241fed8d8c91dd64cb0d8
nimod7890 <nimod1234@g.skku.edu>
2024-04-23 16:08:33 +0900

    11055

--- Silver/Silver 2/11055 가장 큰 증가하는 부분 수열(1).py
+++ Silver/Silver 2/11055 가장 큰 증가하는 부분 수열(1).py
@@ -5,11 +5,11 @@ input=sys.stdin.readline
 n=int(input())
 nlist=[*map(int,input().split())]
 dp=[0]*n
-dp[0]=nlist[0]
 
-for i in range(1,n):
+for i in range(n):
   dp[i]=nlist[i]
   for j in range(i):
     if nlist[i]>nlist[j]:
       dp[i]=max(dp[i],dp[j]+nlist[i])
+      
 print(max(dp))
